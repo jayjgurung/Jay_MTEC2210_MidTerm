6225bbe954a58b35fe93cdabb449d3b7b45e53dc
Jay J <jayjgurung@gmail.com>
2024-02-08 17:24:13 -0500

    Update 2-8-2024

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed =  5;
+    float turboSpeed = 10;
+    float currentSpeed;
+    public Color turboColor;
+    private Color defaultColor;
+    public SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = Color.white;
+       // sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentSpeed = turboSpeed;
+            sr.color = turboColor;
+        }
+        else
+        {
+            currentSpeed = speed;
+            sr.color = defaultColor;
+        }
+
+       
+
+        float xMove = Input.GetAxis("Horizontal");       //Smooth movement
+        float yMove = Input.GetAxis("Vertical");        //Smooth movement
+        transform.Translate(currentSpeed * xMove * Time.deltaTime, currentSpeed * yMove * Time.deltaTime, 0);
+        //Time.deltaTime = makes it frame independent so that different computers dont have different speed
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Item")
+        {
+            Debug.Log("We Collided");
+            defaultColor = collision.gameObject.GetComponent<Item>().itemColor;
+            sr.color = defaultColor;
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+}
